c20b773a9ff084e273ef22037de3a12b56c2ae17
Kayo Phoenix <kayo@illumium.org>
2018-04-02 14:31:19 +0500

    New markdown highlight rules with nesting support

--- src/hl/languages/markdown.ts
+++ src/hl/languages/markdown.ts
@@ -6,7 +6,124 @@ Website: http://seejohncode.com/
 Category: common, markup
 */
 
-import { LanguageDef } from '../types';
+import { LanguageDef, SyntaxDef } from '../types';
+
+// inline links
+const INLINE_LINK_MODE = {
+    begin: '<\\w+:[^>]*>',
+    returnBegin: true,
+    contains: [
+        {
+            className: 'link',
+            begin: '<', end: '>',
+            excludeBegin: true, excludeEnd: true
+        }
+    ],
+    relevance: 5
+};
+
+// inline html
+const INLINE_HTML_MODE = {
+    begin: '<', end: '>',
+    subLanguage: 'xml',
+    relevance: 0
+};
+
+// inline code
+const INLINE_CODE_MODE = {
+    className: 'code',
+    begin: '`.+?`'
+};
+
+const STRONG_CONTAINS: SyntaxDef[] = [
+    INLINE_LINK_MODE,
+    INLINE_HTML_MODE,
+    INLINE_CODE_MODE,
+];
+
+// strong segments
+const STRONG_MODE = {
+    className: 'strong',
+    variants: [
+        {
+            begin: '\\*{2}',
+            end: '\\*{2}',
+            contains: STRONG_CONTAINS
+        },
+        {
+            begin: '_{2}',
+            end: '_{2}',
+            contains: STRONG_CONTAINS
+        },
+    ]
+};
+
+const EMPHASIS_CONTAINS: SyntaxDef[] = [
+    INLINE_LINK_MODE,
+    INLINE_HTML_MODE,
+    INLINE_CODE_MODE,
+];
+
+// emphasis segments
+const EMPHASIS_MODE = {
+    className: 'emphasis',
+    variants: [
+        {
+            begin: '\\*(?!\\*)',
+            end: '\\*(?!\\*)',
+            contains: EMPHASIS_CONTAINS
+        },
+        {
+            begin: '_(?!_)',
+            end: '_(?!_)',
+            contains: EMPHASIS_CONTAINS,
+            relevance: 0,
+        },
+    ]
+};
+
+const INLINE_CONTAINS: SyntaxDef[] = [
+    INLINE_LINK_MODE,
+    INLINE_HTML_MODE,
+    INLINE_CODE_MODE,
+];
+
+// using links - title and link
+const LINK_MODE = {
+    begin: '\\!?\\[.+?\\][\\(\\[].*?[\\)\\]]',
+    returnBegin: true,
+    contains: [
+        {
+            className: 'string',
+            begin: '\\!?\\[', end: '\\]',
+            excludeBegin: true,
+            returnEnd: true,
+            contains: INLINE_CONTAINS,
+            relevance: 0
+        },
+        {
+            className: 'link',
+            begin: '\\]\\(', end: '\\)',
+            excludeBegin: true, excludeEnd: true
+        },
+        {
+            className: 'symbol',
+            begin: '\\]\\[', end: '\\]',
+            excludeBegin: true, excludeEnd: true
+        }
+    ],
+    relevance: 10
+};
+
+STRONG_CONTAINS.push(EMPHASIS_MODE);
+STRONG_CONTAINS.push(LINK_MODE);
+
+EMPHASIS_CONTAINS.push(STRONG_MODE);
+EMPHASIS_CONTAINS.push(LINK_MODE);
+
+INLINE_CONTAINS.push(STRONG_MODE);
+INLINE_CONTAINS.push(EMPHASIS_MODE);
+INLINE_CONTAINS.push(LINK_MODE);
 
 export const Markdown: LanguageDef = {
     name: 'markdown',
@@ -18,39 +135,19 @@ export const Markdown: LanguageDef = {
             variants: [
                 { begin: '^#{1,6}', end: '$' },
                 { begin: '^.+?\\n[=-]{2,}$' }
-            ]
-        },
-        // inline html
-        {
-            begin: '<', end: '>',
-            subLanguage: 'xml',
-            relevance: 0
+            ],
+            contains: INLINE_CONTAINS
         },
         // lists (indicators only)
         {
             className: 'bullet',
             begin: '^\\s*([*+-]|(\\d+\\.))\\s+'
         },
-        // strong segments
-        {
-            className: 'strong',
-            begin: '[*_]{2}.+?[*_]{2}'
-        },
-        // emphasis segments
-        {
-            className: 'emphasis',
-            variants: [
-                { begin: '\\*.+?\\*' },
-                {
-                    begin: '_.+?_'
-                    , relevance: 0
-                }
-            ]
-        },
         // blockquotes
         {
             className: 'quote',
-            begin: '^>\\s+', end: '$'
+            begin: '^>\\s+', end: '$',
+            contains: INLINE_CONTAINS
         },
         // code snippets
         {
@@ -58,9 +155,6 @@ export const Markdown: LanguageDef = {
             variants: [
                 {
                     begin: '^```\w*\s*$', end: '^```\s*$'
-                },
-                {
-                    begin: '`.+?`'
                 },
                 {
                     begin: '^( {4}|\t)', end: '$',
@@ -72,38 +166,13 @@ export const Markdown: LanguageDef = {
         {
             begin: '^[-\\*]{3,}', end: '$'
         },
-        // using links - title and link
-        {
-            begin: '\\[.+?\\][\\(\\[].*?[\\)\\]]',
-            returnBegin: true,
-            contains: [
-                {
-                    className: 'string',
-                    begin: '\\[', end: '\\]',
-                    excludeBegin: true,
-                    returnEnd: true,
-                    relevance: 0
-                },
-                {
-                    className: 'link',
-                    begin: '\\]\\(', end: '\\)',
-                    excludeBegin: true, excludeEnd: true
-                },
-                {
-                    className: 'symbol',
-                    begin: '\\]\\[', end: '\\]',
-                    excludeBegin: true, excludeEnd: true
-                }
-            ],
-            relevance: 10
-        },
         {
             begin: /^\s*\[[^\n]+\]:/,
             returnBegin: true,
             contains: [
                 {
                     className: 'symbol',
-                    begin: /\[/, end: /\]/,
+                    begin: /\s*\[/, end: /\]/,
                     excludeBegin: true, excludeEnd: true
                 },
                 {
@@ -112,6 +181,7 @@ export const Markdown: LanguageDef = {
                     excludeBegin: true
                 }
             ]
-        }
+        },
+        ...INLINE_CONTAINS
     ]
 };
